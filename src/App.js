ca8634f88508c0075fb0a73a8870850c11be921b
gincio <hubokendzior@gmail.com>
2021-07-20 14:51:59 +0200

    [CONFIG] Installed FontAwesome

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg'
 import './App.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSun } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <FontAwesomeIcon icon={ faSun } />
     </div>
   )
 }
